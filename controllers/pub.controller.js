584f947e3f73a85c8159cbd8585d1b851e4bf955
MinhTrung <minhtrung2031997@gmail.com>
2021-10-10 12:02:42 +0700

    first commit

--- controllers/pub.controller.js
+++ controllers/pub.controller.js
@@ -17,5 +17,20 @@ module.exports = {
             ],
             message: "query not successfully"
         });
+    },
+    getAll: async (req, res, next) => {
+        await res.json({
+            result: "Successfully",
+            data: [{
+                name: "trug",
+                age: 18
+            },
+            {
+                name: "trung",
+                age: 19
+            },
+            ],
+            message: "query not successfully"
+        });
     }
 };
--- routes/pub.js
+++ routes/pub.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const tripController = require('../controllers/pub.controller');
 
 router.get('/list_all_pub', tripController.getAllPub);
-
+router.get('/list_pub', tripController.getAll);
 
 module.exports = router;
